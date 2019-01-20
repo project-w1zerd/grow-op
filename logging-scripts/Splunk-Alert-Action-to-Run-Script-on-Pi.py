0700473f53e0824e15024a29bc6bbeacfbf11d8b
project-w1zerd <43553377+project-w1zerd@users.noreply.github.com>
2019-01-20 14:20:37 -0600

    Create Splunk-Alert-Action-to-Run-Script-on-Pi.py

---
+++ logging-scripts/Splunk-Alert-Action-to-Run-Script-on-Pi.py
@@ -0,0 +1,22 @@
+# encoding = utf-8
+# Author: Padraic Canavan
+
+def process_event(helper, *args, **kwargs):
+    import paramiko
+    import time
+
+    cli = paramiko.client.SSHClient()
+    cli.set_missing_host_key_policy(paramiko.client.AutoAddPolicy())
+    cli.connect(hostname="10.0.1.34", username="pi", password="raspberry")
+    stdin_, stdout_, stderr_ = cli.exec_command("python ~/script1.py")
+    # time.sleep(2)    # Previously, I had to sleep for some time.
+    stdout_.channel.recv_exit_status()
+    lines = stdout_.readlines()
+    for line in lines:
+        print line
+
+    cli.close()
+    helper.log_info("Alert action turn_on_device_1 started.")
+
+    # TODO: Implement your alert action logic here
+    return 0
